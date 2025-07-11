7dc2067ebc036f4cd62d4344e782d144b32260cf
Kusal Damsara <kusal2002d@Gmail.com>
2025-07-11 10:24:05 +0530

    Create DataSearch2 Servlet

--- src/java/controller/DataSearch2.java
+++ src/java/controller/DataSearch2.java
@@ -4,12 +4,13 @@ package controller;
 import hibernate.Brand;
 import hibernate.HibernateUtil;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -17,20 +18,21 @@ import org.hibernate.SessionFactory;
  *
  * @author User
  */
-@WebServlet(name = "DataDelete", urlPatterns = {"/DataDelete"})
-public class DataDelete extends HttpServlet {
+@WebServlet(name = "DataSearch2", urlPatterns = {"/DataSearch2"})
+public class DataSearch2 extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         SessionFactory sf = HibernateUtil.getSessionFactory();
         Session s = sf.openSession();
         
-        Brand b = new Brand();
-        b.setId(11);
+        Criteria c = s.createCriteria(Brand.class);  //SELECT * FROM `brand`
+        List<Brand> brandList =  c.list();
         
-        s.delete(b);
-        s.beginTransaction().commit();
+        for (Brand brand : brandList) {
+            System.out.println(brand.getName());
+        }
     }
 
-
+    
 }
